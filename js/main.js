385701d4e44128744449c31ce29a8245e9e347c4
MagicGuySS <52413212+MagicGuySS@users.noreply.github.com>
2020-05-04 15:26:08 +0800

    Moved Js and testing functions for adding elements

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//import React from "react"
+//import ReactDOM from "react-dom"
+
+//ReactDOM.render(
+//    <h1>Hello, world!</h1>,
+//    document.getElementById('root')
+//  );
+
+document.getElementById("btnTxt").onclick = function() {appendText()};
+
+function appendText(){
+  //var txt1 = "<p>Text.</p>";        // Create text with HTML
+  //var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
+  var txt3 = document.createElement("p");
+  txt3.innerHTML = "Text";         // Create text with DOM
+  $("body").append(txt3);   // Append new elements
+}
+
+function appendButton(){
+  var buttonTest = document.createElement("button");
+  buttonTest.innerHTML = "This is a button";
+  $("body").append(buttonTest);   // Append new elements
+}
+
