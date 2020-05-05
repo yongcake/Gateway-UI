e1a40428617c0d59d2a2726e38c4783fb2f0f0a6
MagicGuySS <52413212+MagicGuySS@users.noreply.github.com>
2020-05-05 15:51:52 +0800

    Added removal of items dynamically

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 //document.getElementById("btnTxt").onclick = function() {appendText()};
-
-function appendText(){
-  //var txt1 = "<p>Text.</p>";        // Create text with HTML
-  //var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
-  var txt3 = document.createElement("p");
-  txt3.innerHTML = "Text";         // Create text with DOM
-  $("#testArea").append(txt3);   // Append new elements
-}
-
-function appendButton(){
-  var buttonTest = document.createElement("button");
-  buttonTest.innerHTML = "This is a button";
-  $("#testArea").append(buttonTest);   // Append new elements
-}
+var buttonArray = [], textArray = [], text;
 
 function showCoords(event) {
   var x = event.clientX;
@@ -21,4 +8,82 @@ function showCoords(event) {
   //document.getElementById("instruction").innerHTML = coords;
   alert(coords);
 }
+
+function addText(value){
+  text+= value.id + " "
+}
+
+//Checking Functions
+function checkIDTxt(){ // used to check ids of all txts added
+  text = "";
+  if (textArray.length != 0){
+    textArray.forEach(addText);
+    alert("Current IDs for Text: "+ text);
+  }
+  else
+  {
+    alert("No ID found for Text");
+  }
   
+}
+function checkIDBtn(){ // used to check ids of all buttonss added
+  text = "";
+  if (buttonArray.length != 0){
+    buttonArray.forEach(addText);
+    alert("Current IDs for button = " + text);
+  }
+  else
+  {
+    alert("No ID found for button");
+  }
+}
+//Addings Elements
+function appendText(){
+  //var txt1 = "<p>Text.</p>";        // Create text with HTML
+  //var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
+
+  var txt3 = document.createElement("p");
+  txt3.innerHTML = "Text";         // Create text with DOM
+  txt3.id = "txt" + (textArray.length + 1);
+  textArray.push(txt3);
+  $("#testArea").append(txt3);   // Append new elements
+  checkIDTxt();
+}
+
+function appendButton(){
+  var buttonTest = document.createElement("button");
+  buttonTest.innerHTML = "This is a button";
+  buttonTest.id = "btn" + (buttonArray.length + 1);
+  buttonArray.push(buttonTest);
+  $("#testArea").append(buttonTest);   // Append new elements
+  checkIDBtn();
+}
+
+//Removing Items
+function removeElements(){
+  //alert(textArray);
+  var elementID = document.getElementById("removeID").value;
+  //alert(elementID);
+  var element = document.getElementById(elementID);
+  element.remove();
+  removeText(elementID);
+  removeBtn(elementID);
+  checkIDBtn();
+  checkIDTxt();
+}
+
+function removeText(name){
+  for (i = 0; i < textArray.length; i++){
+    if(textArray[i].id == name){
+      textArray.splice(i,1);
+    }
+  }
+}
+function removeBtn(name){
+  for (i = 0; i < buttonArray.length; i++){
+    if(buttonArray[i].id == name){
+      buttonArray.splice(i,1);
+    }
+  }
+}
+
