75e5e29ee8b2f7d21ec60c1b57b7e2be511f5ccc
MagicGuySS <52413212+MagicGuySS@users.noreply.github.com>
2020-05-05 13:49:47 +0800

    updated the function

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@
 //    document.getElementById('root')
 //  );
 
-document.getElementById("btnTxt").onclick = function() {appendText()};
+//document.getElementById("btnTxt").onclick = function() {appendText()};
 
 function appendText(){
   //var txt1 = "<p>Text.</p>";        // Create text with HTML
   //var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
   var txt3 = document.createElement("p");
   txt3.innerHTML = "Text";         // Create text with DOM
-  $("body").append(txt3);   // Append new elements
+  $("#testArea").append(txt3);   // Append new elements
 }
 
 function appendButton(){
   var buttonTest = document.createElement("button");
   buttonTest.innerHTML = "This is a button";
-  $("body").append(buttonTest);   // Append new elements
+  $("#testArea").append(buttonTest);   // Append new elements
 }
 
