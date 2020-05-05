02f54bbcfe124aed500ee6334b82ac9a2ad1a24f
Yee Yong Khang IT <s10177247@connect.np.edu.sg>
2020-05-05 11:33:36 +0800

    vanilla html update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-//import React from "react"
-//import ReactDOM from "react-dom"
-
-//ReactDOM.render(
-//    <h1>Hello, world!</h1>,
-//    document.getElementById('root')
-//  );
-
 document.getElementById("btnTxt").onclick = function() {appendText()};
 
 function appendText(){
