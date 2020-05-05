58f6603d2c137f2304003173bc79ed35676c5622
Yee Yong Khang IT <s10177247@connect.np.edu.sg>
2020-05-05 13:26:20 +0800

    show coords in img on mouse click with alert

--- js/main.js
+++ js/main.js
@@ -14,3 +14,11 @@ function appendButton(){
   $("body").append(buttonTest);   // Append new elements
 }
 
+function showCoords(event) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var coords = "X coords: " + x + ", Y coords: " + y;
+  //document.getElementById("instruction").innerHTML = coords;
+  alert(coords);
+}
+  
