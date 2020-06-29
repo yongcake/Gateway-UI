91937ba1aa5a3cbb25e6fcafa6e6fa85b82de08a
Yee Yong Khang IT <s10177247@connect.np.edu.sg>
2020-06-29 16:55:08 +0800

    setInterval _should_ work..

--- js/main.js
+++ js/main.js
@@ -54,20 +54,15 @@ $(document).ready( function(){
       gatewayTop = testData["gatewayTop"];
       gatewayFloor = testData["gatewayFloor"];
       floors = testData["floorArray"];
-      $("#imageSource").append("<div class='gateway' id='" + gatewayID + ">'</div>");
+      $("#imageSource").append("<div class='gateway' id='" + gatewayID + "'></div>");
       posX = gatewayLeft * (divWidth/imageWidth) +container.getBoundingClientRect().left -15;
       posY = gatewayTop * (divHeight/imageHeight) +container.getBoundingClientRect().top -15 + window.pageYOffset
       initPlaceMarker($("#"+gatewayID)[0],posX,posY);
-      $("#"+gatewayID).hide();
       for (var j in floors){ //j == Floor'floorNo'
         var floorData = floors[j];
         nodeList = floorData["nodeList"];
         //console.log(floorData);
         var newNodeList = [];
-        //.length to check if already exist
-        if($("#"+j).length == 0 && j != undefined){ //Create the floorSelect button if it doesn't already exist
-        $("#floorSelectionWrapper").append("<input type='button' class='button' id='" + j + "' onclick=switchSites('"+j+"') value='" +j+ "'></input>");
-        }
         for (var k in nodeList){
           var nodeData = nodeList[k];
          // console.log(nodeData);
@@ -88,6 +83,10 @@ $(document).ready( function(){
             var n = new Node(markerID, nodeID, location, nodeName, infoID,area,pointID,active); //create new node according to json
             n.status = status;
             //posLeft = posLeft *(divWidth/imageWidth) +container.getBoundingClientRect().left -15  ; //Downscale to where it should be on the container
+
+            if($("#"+area).length == 0 && area != undefined){ //Create the floorSelect button if it doesn't already exist
+              $("#floorSelectionWrapper").append("<input type='button' class='button' id='" + area + "' onclick=switchSites('"+area+"') value='" +area+ "'></input>");
+            }
             n.updatePosition(posLeft, posTop);
 
             createNodeContainer(n); // create node container (info) according to json
@@ -139,12 +138,11 @@ $(document).ready( function(){
 
     if (testArray[0]['floorArray'][0] != undefined){ //check if there is any site added
       currentFloor = testArray[0]['floorArray'][0][0];
-      switchSites(currentFloor);
+     switchSites(currentFloor);
     }
     else{
       currentFloor = "Floor1";
-      switchSites(currentFloor);
-      $("#floorSelectionWrapper").append("<input type='button' class='button' id='Floor1' onclick=switchSites('Floor1') value='Floor1'></input>"); //Remove this after modifying
+        $("#floorSelectionWrapper").append("<input type='button' class='button' id='Floor1' onclick=switchSites('Floor1') value='Floor1'></input>"); //Remove this after modifying
     }
     //Site Array format [[*SiteName*,*NodeArrays[*nodes*]*],[*SiteName*,*NodeArrays[*nodes*]*]]
     /*for (i = 0; i< floorArray.length;i++){
@@ -263,6 +261,7 @@ function removeUnwantedMarker(){
 
 function addGatewayPressed(){
   addGatewayButtonPressed = true;
+  currentFloor = "Floor1";
   testCounter++;
   var testNo = "Test"+testCounter;
   var gatewayID = "gateway"+testCounter;
@@ -1043,9 +1042,6 @@ function switchSites(newSite){ //Toggle between Sites
 function remapMarkers(newSite){
   for(var i = 0;i<testArray.length;i++){ //loop all the sites
     if(!testArray[i].testCompleted){
-      if(testArray[i].gatewayFloor == newSite){
-        $("#"+testArray[i].gatewayID).show();
-      }
       for(j = 0; j <testArray[i].floorArray.length;j++){
         if(testArray[i].floorArray[i][1] !== undefined){ //in case site is created but no markers was added
           if(testArray[i].floorArray[j][0]== newSite){
@@ -1066,9 +1062,6 @@ function remapMarkers(newSite){
 function clearSite(currentFloor){
   for(var i = 0;i<testArray.length;i++){ //loop all the sites
     if(!testArray[i].testCompleted){
-      if(testArray[i].gatewayFloor == currentFloor){
-        $("#"+testArray[i].gatewayID).hide();
-      }
       for(j = 0; j <testArray[i].floorArray.length;j++){
         if(testArray[i].floorArray[i][1] !== undefined){ //in case site is created but no markers was added
           if(testArray[i].floorArray[j][0]== currentFloor){
@@ -1136,13 +1129,17 @@ function updateSignal(){
       var floorArrayData = testData["floorArray"];  //floorArray in Test
       for (var j in floorArrayData){ 
         if (j == currentFloor){
-          var nodeListData = floorArrayData["nodeList"]; //each points in nodeList
+          //console.log(j);
+          var nodeListData = floorArrayData[j]["nodeList"]; //each points in nodeList
+          //console.log(nodeListData); 
           for (var k = 0; k<nodeList.length; k++){
             for (l in nodeListData){
-              if (nodeList[k].pointID == l){
+              //console.log(l.toLowerCase());
+              if (nodeList[k].pointID == l.toLowerCase()){
                 nodeList[k].signal = nodeListData[l]["signal"];
+                console.log(nodeList[k].signal);
                 nodeList[k].statusChange();
-                $("#"+ nodeList[k].infoID).html(nodeList[i].print());
+                $("#"+ nodeList[k].infoID).html(nodeList[k].print());
                 changeSignalStrengthNotation(nodeList[k].markerID);
                 console.log("reading from json and printing info out");
               }
@@ -1158,11 +1155,3 @@ $("document").ready(function(){
 setInterval(updateSignal, 1000);
 }); 
 
-function testUpdate(){
-  var filePath = "./getActiveItems.php";
-  var searchKey = "signal";
-  $.ajaxSetup({cache:false}); //disable cache so it can update 
-  $.get(filePath, function(data){
-    console.log(data);
-  }); 
-}
